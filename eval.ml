0811e523d0f78c9cab430d3138fd65815c89f63d
Phil Armitage <philip.armitage@gmail.com>
2010-12-20 22:49:20 +0000

    basic math operators

--- eval.ml
+++ eval.ml
@@ -6,6 +6,7 @@
    - Move modules into separate files
    - apply needs to evaluate where clause
    - evlis is inefficient - doesn't need to evaluate all lists
+   - Does OCaml intern strings or should I intern symbols?
 *)
 
 exception Type_mismatch
@@ -21,6 +22,9 @@ type expression =
   | Closure of expression list * expression * expression * binding list
   | Apply of expression * expression list
   | Add of expression * expression
+  | Sub of expression * expression
+  | Mul of expression * expression
+  | Div of expression * expression
 and binding = { symbol: string; value: expression; }
 
 (* pretty printer *)
@@ -31,8 +35,11 @@ let rec pprint exp =
     | Float f -> print_float f; exp
     | Char c -> print_char c; exp
     | List l -> exp
-    | Closure(args, body, where, env) -> print_string "#<closure>"; exp
-    | Add(lhs, rhs) -> print_string "#<operator>"; exp
+    | Closure(p, b, w, e) -> print_string "#<closure>"; exp
+    | Add(x, y) -> print_string "#<operator>"; exp
+    | Sub(x, y) -> print_string "#<operator>"; exp
+    | Mul(x, y) -> print_string "#<operator>"; exp
+    | Div(x, y) -> print_string "#<operator>"; exp
     | Apply(s, a) -> print_string "#<funcall>"; exp
 
 let rec take_while p lst = match lst with 
@@ -62,7 +69,10 @@ struct
     else {symbol = s; value = v} :: e
   let create env = {symbol = "env"; value = Int(-1)} :: env
   let bind p a e =
-    List.append (List.map2 (fun (Symbol s) a -> {symbol = s; value = a}) p a) e
+    List.append (List.map2
+                   (fun sym a -> match sym with
+                       Symbol s -> {symbol = s; value = a}
+                     | _ -> raise Type_mismatch) p a) e
 end
 
 module Operators =
@@ -73,6 +83,27 @@ struct
       | Int x, Float y -> Float(float_of_int x +. y)
       | Float x, Int y -> Float(x +. float_of_int y)
       | Float x, Float y -> Float(x +. y)
+      | _ -> raise Type_mismatch
+  let sub lhs rhs =
+    match lhs, rhs with
+        Int x, Int y -> Int(x - y)
+      | Int x, Float y -> Float(float_of_int x -. y)
+      | Float x, Int y -> Float(x -. float_of_int y)
+      | Float x, Float y -> Float(x -. y)
+      | _ -> raise Type_mismatch
+  let mul lhs rhs =
+    match lhs, rhs with
+        Int x, Int y -> Int(x * y)
+      | Int x, Float y -> Float(float_of_int x *. y)
+      | Float x, Int y -> Float(x *. float_of_int y)
+      | Float x, Float y -> Float(x *. y)
+      | _ -> raise Type_mismatch
+  let div lhs rhs =
+    match lhs, rhs with
+        Int x, Int y -> Float(float_of_int x /. float_of_int y)
+      | Int x, Float y -> Float(float_of_int x /. y)
+      | Float x, Int y -> Float(x /. float_of_int y)
+      | Float x, Float y -> Float(x /. y)
       | _ -> raise Type_mismatch
 end
 
@@ -87,6 +118,9 @@ let rec eval exp env =
     | Closure(args, body, where, env) -> exp
     | Apply(s, a) -> apply s (evlis a env) env
     | Add(lhs, rhs) -> Operators.add (eval lhs env) (eval rhs env)
+    | Sub(lhs, rhs) -> Operators.sub (eval lhs env) (eval rhs env)
+    | Mul(lhs, rhs) -> Operators.mul (eval lhs env) (eval rhs env)
+    | Div(lhs, rhs) -> Operators.div (eval lhs env) (eval rhs env)
 and apply sym args e =
   match sym with
       Symbol s ->
@@ -105,13 +139,11 @@ let x = { symbol = "x"; value = Int(400) } ;;
 let y = { symbol = "y"; value = Float(3.14) } ;;
 
 (* TODO try adding symbols *)
-let body = Add(Symbol("x"), Int(12)) ;;
+let body = Mul(Symbol("x"), Int(12)) ;;
 let func = Closure(Symbol("x") :: [], body, Symbol("a"), []) ;;
 let f1 = { symbol = "f1"; value = func } ;;
-let a = Apply(Symbol("f1"), Symbol("x") :: []) ;;
+let a = Apply(Symbol("f1"), Symbol("y") :: []) ;;
 
 let e = f1 :: x :: y :: [] ;;
 let result = eval a e ;;
 pprint(result) ;;
-
-Environment.bind (Symbol("x") :: Symbol("y") :: []) (Int(2) :: Float(1.2) :: []) e
