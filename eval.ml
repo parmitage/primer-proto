26ca5b05d5da9a2345198ddd902d398fe69ec36c
Phil Armitage <philip.armitage@gmail.com>
2010-12-20 20:34:10 +0000

    basic modules

--- eval.ml
+++ eval.ml
@@ -1,11 +1,11 @@
-(* TODO
+(* TODO / NOTE
    - Closure type ctor should take a list of expressions as 1st parameter
    - Apply type ctor should take a list of expressions as 2nd parameter
    - Association list for bindings / environments?
    - Put take_while in a utils module? In its own file?
-   - Should all environment related functions go into a module?
-   - environment_lookup is inefficient as it does a double scan of the environment
+   - Environment.lookup is inefficient as it does a double scan of the environment
    - Nicer marker for environment frames
+   - Move modules into separate files
    - Apply case in eval needs to evaluate its arguments
    - apply needs to bind arguments
    - apply needs to evaluate where clause
@@ -43,65 +43,59 @@ let rec take_while p lst = match lst with
   | [] -> []
   | x::xs -> if p x then x :: (take_while p xs) else []
 
-(* is a symbol represented by this binding *)
-let symbol_eq sym bind = match bind with {symbol; value} -> symbol = sym
+module Environment =
+struct
+  let symbol_eq sym bind = match bind with {symbol; value} -> symbol = sym
+  let symbol_bound sym env = 
+    try
+      match List.find (symbol_eq sym) env with
+          {symbol; value} -> true
+    with
+        Not_found -> false
+  let lookup sym env =
+    if symbol_bound sym env
+    then match List.find (symbol_eq sym) env with {symbol; value} -> value
+    else raise Unbound_symbol
+  let top env =
+    take_while (fun b -> match b with {symbol; value} -> symbol <> "env") env
+  let extend e s v =
+    let current = top e in
+    let bound = symbol_bound s current in
+    if bound
+    then raise Already_bound
+    else {symbol = s; value = v} :: e
+  let create env = {symbol = "env"; value = Int(-1)} :: env
+end
 
-(* is a symbol bound in an environment *)
-let symbol_bound sym env = 
-  try
-    match List.find (symbol_eq sym) env with
-        {symbol; value} -> true
-  with
-      Not_found -> false
+module Operators =
+struct
+  let add lhs rhs =
+    match lhs, rhs with
+        Int x, Int y -> Int(x + y)
+      | Int x, Float y -> Float(float_of_int x +. y)
+      | Float x, Int y -> Float(x +. float_of_int y)
+      | Float x, Float y -> Float(x +. y)
+      | _ -> raise Type_mismatch
+end
 
-(* return the value bound to a symbol in an environment *)
-let environment_lookup sym env =
-  if symbol_bound sym env
-  then match List.find (symbol_eq sym) env with
-      {symbol; value} -> value
-  else raise Unbound_symbol
-
-(* return the most inner environment *)
-let environment_top env =
-  take_while (fun b -> match b with {symbol; value} -> symbol <> "env") env
-
-(* add a binding to an environment *)
-let environment_extend e s v =
-  let current = environment_top e in
-  let bound = symbol_bound s current in
-  if bound
-  then raise Already_bound
-  else {symbol = s; value = v} :: e
-
-(* add an environment marker to an existing environment *)
-let environment_new env = {symbol = "env"; value = Int(-1)} :: env
-
-let add lhs rhs =
-  match lhs, rhs with
-      Int x, Int y -> Int(x + y)
-    | Int x, Float y -> Float(float_of_int x +. y)
-    | Float x, Int y -> Float(x +. float_of_int y)
-    | Float x, Float y -> Float(x +. y)
-    | _ -> raise Type_mismatch
-
-(* evaluate an expression in an environment  *)
+(* evaluate an expression in an environment *)
 let rec eval exp env =
   match exp with
-      Symbol s -> environment_lookup s env
+      Symbol s -> Environment.lookup s env
     | Int i -> exp
     | Float f -> exp
     | Char c -> exp
     | List l -> List(evlis l env)
     | Closure(args, body, where, env) -> exp
     | Apply(s, a) -> apply s a env
-    | Add(lhs, rhs) -> add (eval lhs env) (eval rhs env)
+    | Add(lhs, rhs) -> Operators.add (eval lhs env) (eval rhs env)
 and apply sym args e =
   match sym with
       Symbol s ->
-        let f = environment_lookup s e in
+        let f = Environment.lookup s e in
         begin match f with
             Closure(a, b, w, ce) ->
-              eval b (environment_new ce)
+              eval b (Environment.create ce)
           | _ -> raise Type_mismatch
         end
     | _ -> raise Type_mismatch
