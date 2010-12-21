d077de367c4d92d48a365bd7796d16c07e41ab2d
Phil Armitage <philip.armitage@gmail.com>
2010-12-21 17:13:55 +0000

    unified binding and definition

--- eval.ml
+++ eval.ml
@@ -1,10 +1,8 @@
 (* TODO / NOTE
-   - Association list for bindings / environments?
-   - Put take_while in a utils module? In its own file?
+   - Better matching for symbols and definitions (symbol_eq isn't really symbol equality)
    - Environment.lookup is inefficient as it does a double scan of the environment
    - Nicer marker for environment frames
-   - Move modules into separate files
-   - apply needs to evaluate where clause
+   - apply needs to intern definitions in where clause
    - evlis is inefficient - doesn't need to evaluate all lists
    - Does OCaml intern strings or should I intern symbols?
 *)
@@ -19,15 +17,14 @@ type expression =
   | Float of float
   | Char of char
   | List of expression list
-  | Closure of expression list * expression * expression * binding list
+  | Closure of expression list * expression * definition list * definition list
   | Apply of expression * expression list
   | Add of expression * expression
   | Sub of expression * expression
   | Mul of expression * expression
   | Div of expression * expression
-and binding = { symbol: string; value: expression; }
+and definition = Def of expression * expression
 
-(* pretty printer *)
 let rec pprint exp =
   match exp with
       Symbol s -> print_string s; exp
@@ -48,34 +45,39 @@ let rec take_while p lst = match lst with
 
 module Environment =
 struct
-  let symbol_eq sym bind = match bind with {symbol; value} -> symbol = sym
+  let symbol_eq sym def =
+    match def with
+        Def(s, v) -> match s, sym with
+            Symbol str1, Symbol str2 -> str1 = str2
+          | _ -> raise Type_mismatch
   let symbol_bound sym env = 
     try
       match List.find (symbol_eq sym) env with
-          {symbol; value} -> true
+          Def(s, v) -> true
     with
         Not_found -> false
   let lookup sym env =
     if symbol_bound sym env
-    then match List.find (symbol_eq sym) env with {symbol; value} -> value
+    then match List.find (symbol_eq sym) env with Def(s, v) -> v
     else raise Unbound_symbol
   let top env =
-    take_while (fun b -> match b with {symbol; value} -> symbol <> "env") env
+    take_while
+      (fun b -> match b with
+          Def(s, v) -> match s with
+              Symbol str -> str <> "env"
+            | _ -> raise Type_mismatch) env
   let extend e s v =
     let current = top e in
     let bound = symbol_bound s current in
     if bound
     then raise Already_bound
-    else {symbol = s; value = v} :: e
-  let create env = {symbol = "env"; value = Int(-1)} :: env
+    else Def(s, v) :: e
+  let create env = Def(Symbol "env", Int(-1)) :: env
   let bind p a e =
-    List.append (List.map2
-                   (fun sym a -> match sym with
-                       Symbol s -> {symbol = s; value = a}
-                     | _ -> raise Type_mismatch) p a) e
+    List.append (List.map2 (fun sym a -> Def(sym, a)) p a) e
 end
 
-module Operators =
+module Operator =
 struct
   let add lhs rhs =
     match lhs, rhs with
@@ -107,41 +109,48 @@ struct
       | _ -> raise Type_mismatch
 end
 
-(* evaluate an expression in an environment *)
 let rec eval exp env =
   match exp with
-      Symbol s -> Environment.lookup s env
+      Symbol s -> Environment.lookup exp env
     | Int i -> exp
     | Float f -> exp
     | Char c -> exp
     | List l -> List(evlis l env)
     | Closure(args, body, where, env) -> exp
     | Apply(s, a) -> apply s (evlis a env) env
-    | Add(lhs, rhs) -> Operators.add (eval lhs env) (eval rhs env)
-    | Sub(lhs, rhs) -> Operators.sub (eval lhs env) (eval rhs env)
-    | Mul(lhs, rhs) -> Operators.mul (eval lhs env) (eval rhs env)
-    | Div(lhs, rhs) -> Operators.div (eval lhs env) (eval rhs env)
+    | Add(lhs, rhs) -> Operator.add (eval lhs env) (eval rhs env)
+    | Sub(lhs, rhs) -> Operator.sub (eval lhs env) (eval rhs env)
+    | Mul(lhs, rhs) -> Operator.mul (eval lhs env) (eval rhs env)
+    | Div(lhs, rhs) -> Operator.div (eval lhs env) (eval rhs env)
 and apply sym args e =
   match sym with
       Symbol s ->
-        let f = Environment.lookup s e in
+        let f = Environment.lookup sym e in
         begin match f with
             Closure(p, b, w, ce) ->
               eval b (Environment.bind p args (Environment.create ce))
           | _ -> raise Type_mismatch
         end
     | _ -> raise Type_mismatch
-and evlis lst env =
-  List.map (fun exp -> eval exp env) lst
+and evlis lst env = List.map (fun exp -> eval exp env) lst
+
+(*
+  Ideally the parser should generate two lists:
+
+  1. A list of top level definitions. These will be turned into bindings and
+  interned in the top level environment.
+
+  2. A list of expressions. Each one of these should be evaluated in turn in
+  the newly created top level environment.
+*)
 
 (* test program *)
-let x = { symbol = "x"; value = Int(400) } ;;
-let y = { symbol = "y"; value = Float(3.14) } ;;
+let x = Def(Symbol("x"), Int(400)) ;;
+let y = Def(Symbol("y"), Float(3.14)) ;;
 
-(* TODO try adding symbols *)
 let body = Mul(Symbol("x"), Int(12)) ;;
-let func = Closure(Symbol("x") :: [], body, Symbol("a"), []) ;;
-let f1 = { symbol = "f1"; value = func } ;;
+let func = Closure(Symbol("x") :: [], body, [], []) ;;
+let f1 = Def(Symbol("f1"), func) ;;
 let a = Apply(Symbol("f1"), Symbol("y") :: []) ;;
 
 let e = f1 :: x :: y :: [] ;;
